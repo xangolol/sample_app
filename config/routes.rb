30eb98feba15502ed7edf60156b0798e7c5cf34f
xangolol <superxango@hotmail.com>
2013-08-10 20:01:30 +0200

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 SampleApp::Application.routes.draw do
-  resources :user
+  resources :users
   root  'static_pages#home'
-  match '/signup',  to: 'user#new',            via: 'get'
+  match '/signup',  to: 'users#new',            via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
